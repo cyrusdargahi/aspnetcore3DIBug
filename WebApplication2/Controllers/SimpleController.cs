318cfd7f5b12036825f070b130ab76572d9ca75a
Cyrus <cyrus.dargahi@splitthepot.se>
2019-10-01 20:49:18 +0200

    initial commit with example of what is not working

---
+++ WebApplication2/Controllers/SimpleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Service;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+        private readonly SimpleService simpleService;
+
+        public SimpleController(SimpleService simpleService)
+        {
+            this.simpleService = simpleService;
+        }
+
+        [HttpGet]
+        public decimal Get()
+        {
+            return simpleService.Value;
+        }
+    }
+}
---
+++ WebApplication2/Service/SimpleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Service
+{
+    public class SimpleService
+    {
+        public SimpleService(decimal value = 0.01m)
+        {
+            Value = value;
+        }
+
+        public decimal Value { get; }
+    }
+}
